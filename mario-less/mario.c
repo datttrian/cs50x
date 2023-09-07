d13118f5d58732001a1c28028660211c209b7f25
Dat Tran <datttrian@gmail.com>
2023-09-07 20:03:43 +0000

    Thu, Sep 7, 2023, 8:03 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,6 +1,15 @@
 #include <cs50.h>
 #include <stdio.h>
 
+// This function repeats a character 'c' 'times' number of times and prints them.
+void repeat(char c, int times)
+{
+    while (--times >= 0)
+    {
+        printf("%c", c);
+    }
+}
+
 int main(void)
 {
     int n;
@@ -11,19 +20,13 @@ int main(void)
     while (n < 1 || n > 8); // keep asking until the input is between 1 and 8 (inclusive)
 
     // This loop is used to print the rows of the pyramid
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
-        // This loop is used to print spaces before the hashes
-        for (int j = 0; j < n - i - 1; j++)
-        {
-            printf(" ");
-        }
+        // Print spaces before the hashes
+        repeat(' ', n - i);
 
-        // This loop is used to print the hashes
-        for (int j = 0; j < i + 1; j++)
-        {
-            printf("#");
-        }
+        // Print the hashes
+        repeat('#', i);
 
         printf("\n"); // move to the next line after printing each row
     }
