50aece40586fb984445575f47efb92cc0ce38672
Dat Tran <datttrian@gmail.com>
2023-09-07 22:21:03 +0000

    Thu, Sep 7, 2023, 10:21 PM +00:00

--- no-vowels/no-vowels.c
+++ no-vowels/no-vowels.c
@@ -5,8 +5,42 @@
 
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+
+string replace(string input_string);
 
 int main(int argc, string argv[])
 {
-    
+    if (argc != 2)
+    {
+        printf("Usage: ./no-vowels word\n"); // print usage message if the number of arguments is not 2
+        return 1;                            // return with an error code
+    }
+    printf("%s\n", replace(argv[1])); // print the result of calling the 'replace' function with the argument
+}
+
+string replace(string input_string)
+{
+    int length = strlen(input_string); // get the length of the input string
+
+    for (int i = 0; i < length; i++) // iterate through each character in the input string
+    {
+        switch (input_string[i]) // check the current character
+        {
+            case 'a':
+                input_string[i] = 54; // replace 'a' with ASCII code 54 ('6')
+                break;
+            case 'e':
+                input_string[i] = 51; // replace 'e' with ASCII code 51 ('3')
+                break;
+            case 'i':
+                input_string[i] = 49; // replace 'i' with ASCII code 49 ('1')
+                break;
+            case 'o':
+                input_string[i] = 48; // replace 'o' with ASCII code 48 ('0')
+                break;
+        }
+    }
+
+    return input_string; // return the modified input string
 }
