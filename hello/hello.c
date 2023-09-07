fd8095a0e020d083093e98a46d901210b0b30dd3
Dat Tran <datttrian@gmail.com>
2023-09-07 18:47:13 +0000

    Thu, Sep 7, 2023, 6:47 PM +00:00

--- hello/hello.c
+++ hello/hello.c
@@ -3,6 +3,6 @@
 
 int main(void)
 {
-    string name = get_string("What's your name? ");
-    printf("hello, %s\n", name);
+    string name = get_string("What's your name? "); // prompt the user to input their name using cs50 function.
+    printf("hello, %s\n", name); // print a greeting message including the user's inputted name.
 }
