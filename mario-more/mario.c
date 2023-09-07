8ad2d284c215ecc48cc7976a06e992e62aaac427
Dat Tran <datttrian@gmail.com>
2023-09-07 19:15:02 +0000

    Thu, Sep 7, 2023, 7:15 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,46 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    int n;
+    do
+    {
+        n = get_int("Positive Number: ");
+    }
+    while (n < 1 || n > 8);
+
+    for (int i = 1; i <= n; i++)
+    {
+
+        for (int j = 1; j <= n; j++)
+        {
+            if (j <= n - i)
+            {
+
+                printf(" ");
+            }
+            else
+            {
+
+                printf("#");
+            }
+        }
+
+        printf("  ");
+
+        for (int k = n; k >= 1; k--)
+        {
+            if (k <= n - i)
+            {
+            }
+            else
+            {
+
+                printf("#");
+            }
+        }
+
+        printf("\n");
+    }
 }
