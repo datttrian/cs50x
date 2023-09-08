7d70454358fd025fa8e278d6b133a9e1fe18e5f6
Dat Tran <datttrian@gmail.com>
2023-09-08 15:19:14 +0000

    Fri, Sep 8, 2023, 3:19 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -3,60 +3,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-bool only_digits(string str);
-char rotate(char c, int key);
-
 int main(int argc, string argv[])
 {
-    // Check for the correct number of command-line arguments
+    // Make sure the program was run with just one command-line argument
     if (argc != 2)
     {
         printf("Usage: ./caesar key\n");
         return 1;
     }
 
-    // Check if argv[1] contains only digits
-    if (!only_digits(argv[1]))
+    // Make sure every character in argv[1] is a digit
+    string key_str = argv[1];
+    for (int i = 0; key_str[i] != '\0'; i++)
     {
-        printf("Usage: ./caesar key\n");
-        return 1;
+        if (!isdigit(key_str[i]))
+        {
+            printf("Usage: ./caesar key\n");
+            return 1;
+        }
     }
 
-    // Convert the key from a string to an integer
-    int key = atoi(argv[1]);
+    // Convert argv[1] from a `string` to an `int`
+    int key = atoi(key_str);
 
     // Prompt the user for plaintext
     string plaintext = get_string("plaintext: ");
 
-    // Encrypt and print the ciphertext
+    // For each character in the plaintext:
     printf("ciphertext: ");
     for (int i = 0; plaintext[i] != '\0'; i++)
     {
-        printf("%c", rotate(plaintext[i], key));
+        // Rotate the character if it's a letter
+        char rotated_char = plaintext[i];
+        if (isalpha(plaintext[i]))
+        {
+            char base = islower(plaintext[i]) ? 'a' : 'A';
+            rotated_char = (rotated_char - base + key) % 26 + base;
+        }
+        printf("%c", rotated_char);
     }
     printf("\n");
 
     return 0;
-}
-
-bool only_digits(string str)
-{
-    for (int i = 0; str[i] != '\0'; i++)
-    {
-        if (!isdigit(str[i]))
-        {
-            return false;
-        }
-    }
-    return true;
-}
-
-char rotate(char c, int key)
-{
-    if (isalpha(c))
-    {
-        char base = islower(c) ? 'a' : 'A';
-        return (c - base + key) % 26 + base;
-    }
-    return c;
 }
