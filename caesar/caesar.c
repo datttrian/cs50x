be1957eed823b4b47db634ce64b7bc40069edee9
Dat Tran <datttrian@gmail.com>
2023-09-08 15:00:27 +0000

    Fri, Sep 8, 2023, 3:00 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,7 +1,62 @@
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, string argv[]) 
+bool only_digits(string str);
+char rotate(char c, int key);
+
+int main(int argc, string argv[])
 {
+    // Check for the correct number of command-line arguments
+    if (argc != 2)
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
 
+    // Check if argv[1] contains only digits
+    if (!only_digits(argv[1]))
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+
+    // Convert the key from a string to an integer
+    int key = atoi(argv[1]);
+
+    // Prompt the user for plaintext
+    string plaintext = get_string("plaintext: ");
+
+    // Encrypt and print the ciphertext
+    printf("ciphertext: ");
+    for (int i = 0; plaintext[i] != '\0'; i++)
+    {
+        printf("%c", rotate(plaintext[i], key));
+    }
+    printf("\n");
+
+    return 0;
+}
+
+bool only_digits(string str)
+{
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (!isdigit(str[i]))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+char rotate(char c, int key)
+{
+    if (isalpha(c))
+    {
+        char base = islower(c) ? 'a' : 'A';
+        return (c - base + key) % 26 + base;
+    }
+    return c;
 }
