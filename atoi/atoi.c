e62d2cc4cde9a98ee0fd0a476816dcb9cd213b44
Dat Tran <datttrian@gmail.com>
2023-09-08 20:28:02 +0000

    Fri, Sep 8, 2023, 8:28 PM +00:00

--- atoi/atoi.c
+++ atoi/atoi.c
@@ -26,4 +26,19 @@ int main(void)
 int convert(string input)
 {
     // TODO
+    // Base case: If the string is empty or has only one character (null terminator),
+    // return the integer value of that character.
+    if (str[0] == '\0' || str[1] == '\0') {
+        return str[0] - '0';
+    }
+
+    // Recursively calculate the integer value of the substring without the last character.
+    int smallerValue = convert(str + 1);
+
+    // Calculate the integer value of the current character and combine it with the
+    // value from the smaller substring.
+    int currentValue = str[0] - '0';
+    int result = currentValue * (int)pow(10, strlen(str) - 1) + smallerValue;
+
+    return result;
 }
