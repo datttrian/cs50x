dbe862cec245d06fc594d3377645c77b89da3684
Dat Tran <datttrian@gmail.com>
2023-09-09 01:12:07 +0000

    Sat, Sep 9, 2023, 1:12 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -127,25 +127,38 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
-    // TODO
-    // Check if the name is a valid candidate name
-    for (int i = 0; i < candidate_count; i++)
+  // TODO
+  // Check if the name is a valid candidate name
+  for (int i = 0; i < candidate_count; i++)
+  {
+    if (strcmp(name, candidates[i].name) == 0)
     {
-        if (strcmp(name, candidates[i].name) == 0)
-        {
-            // Update the preferences array
-            preferences[voter][rank] = 1;
-            return true;
-        }
+      // Update the preferences array
+      preferences[voter][rank] = 1;
+      return true;
     }
-    return false; // invalid candidate name
+  }
+  return false; // invalid candidate name
 }
 
 // Tabulate votes for non-eliminated candidates
 void tabulate(void)
 {
-    // TODO
-    return;
+  // TODO
+  for (int i = 0; i < voter_count; i++)
+  {
+    for (int j = 0; j < candidate_count; j++)
+    {
+      int preferred_candidate_index = preferences[i][j];
+
+      // Check if the preferred candidate is eliminated
+      if (!candidates[preferences_candidate_index].eliminated)
+      {
+        candidates[preferred_candidate_index].votes++;
+        break;
+      }
+    }
+  }
 }
 
 // Print the winner of the election, if there is one
