d97c91407f30fc2b5bc8b5c6a6a21e115bb5c581
Dat Tran <datttrian@gmail.com>
2023-09-09 21:15:11 +0000

    Sat, Sep 9, 2023, 9:15 PM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -3,17 +3,15 @@ from cs50 import get_int
 
 def main():
     while True:
-        height = get_int("Height: ")
-        if height >= 1 and height <= 8:
+        n = get_int("Height: ")
+        if n >= 1 and n <= 8:
             break
 
-    for i in range(1, height + 1):
-        num_hashes = i + 1
-        num_spaces = width - num_hashes
-
-        print(" " * num_spaces, end="")
-        print("#" * num_hashes)
+    for row in range(1, n + 1):
+        print(" " * (n-row), end="")
+        print("#" * row)
 
 
 if __name__ == "__main__":
     main()
+    
