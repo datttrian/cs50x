1efdaf5e4cfdb64fcb6c7e0540c115d0dcef9e9d
Dat Tran <datttrian@gmail.com>
2023-09-09 21:17:46 +0000

    Sat, Sep 9, 2023, 9:17 PM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,17 +1,22 @@
 # TODO
 from cs50 import get_int
 
+
 def main():
+    # Display "Height: " to prompt the user input a positive integer between 1 and 8 and store it in n
     while True:
         n = get_int("Height: ")
         if n >= 1 and n <= 8:
             break
 
+    # For each row from '1' to 'n'
     for row in range(1, n + 1):
-        print(" " * (n-row), end="")
+        # Print 'n - row' spaces
+        print(" " * (n - row), end="")
+
+        # Print 'row' hash symbols ('#') for the pyramid
         print("#" * row)
 
 
 if __name__ == "__main__":
     main()
-    
