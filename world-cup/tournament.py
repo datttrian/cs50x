6e753d0299e24e0a6a8026506eb7526f1a05c73d
Dat Tran <datttrian@gmail.com>
2023-09-09 10:42:56 +0000

    Sat, Sep 9, 2023, 10:42 AM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -5,7 +5,7 @@ import sys
 import random
 
 # Number of simluations to run
-N = 1000
+N = 1000000
 
 
 def main():
@@ -21,7 +21,6 @@ def main():
         for team in reader:
             team["rating"] = int(team["rating"])
             teams.append(team)
-
 
     # Keep track of number of wins for each team
     counts = {}
