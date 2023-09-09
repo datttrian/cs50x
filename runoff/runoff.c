16f1829a737263b5ff7587d5b7098a5c2b142a56
Dat Tran <datttrian@gmail.com>
2023-09-09 01:08:34 +0000

    Sat, Sep 9, 2023, 1:08 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -187,13 +187,26 @@ int find_min(void)
 // Return true if the election is tied between all candidates, false otherwise
 bool is_tie(int min)
 {
-    // TODO
-    for
+  // TODO
+  for (int i = 0; i < candidate_count; i++)
+  {
+    if (!candidates[i].eliminated && candidates[i].votes != min)
+    {
+      return false;
+    }
+  }
+  return true;
 }
 
 // Eliminate the candidate (or candidates) in last place
 void eliminate(int min)
 {
-    // TODO
-    return;
+  // TODO
+  for (int i = 0; i < candidate_count; i++)
+  {
+    if (candidates[i].votes == min)
+    {
+      candidates[i].eliminated = true;
+    }
+  }
 }
