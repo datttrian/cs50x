6f99c87f53abef4b8ad098c83d8a6adcbd1fe14b
Dat Tran <datttrian@gmail.com>
2023-09-09 16:52:20 +0000

    Sat, Sep 9, 2023, 4:52 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -16,14 +16,14 @@ int main(int argc, string argv[])
         // If the argument count is not 2, print an error message
         printf("Usage: %s key\n", argv[0]);
 
-        // Exit with an error code 1
+        // Exit with an error code (1)
         return 1;
     }
 
     // Store the second command-line argument as a string
     string key_str = argv[1];
 
-    // Loop through each character in the argument:
+    // Loop through each character in the argument string
     for (int i = 0; i < strlen(key_str); i++)
     {
         // If it's not a digit
@@ -32,7 +32,7 @@ int main(int argc, string argv[])
             // Print an error message ("Usage: program_name key")
             printf("Usage: ./caesar key\n");
 
-            // Exit with an error code 1
+            // Exit with an error code (1)
             return 1;
         }
     }
