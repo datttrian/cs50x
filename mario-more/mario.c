672fc793ad2f8c4f19265eee8e75ab0957fe19e6
Dat Tran <datttrian@gmail.com>
2023-09-09 13:54:19 +0000

    Sat, Sep 9, 2023, 1:54 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,40 +1,42 @@
 #include <cs50.h>
 #include <stdio.h>
 
-void repeat(char c, int times);
-
 int main(void)
 {
     int n;
     do
     {
-        n = get_int("Positive Number: "); // prompt the user for a positive integer
+        n = get_int("Height: ");
     }
     while (n < 1 || n > 8); // keep asking until the input is between 1 and 8 (inclusive)
 
-    // This loop is used to print the rows of the pyramid
+    // Outer loop for rows
     for (int i = 1; i <= n; i++)
     {
-        // Print n - i spaces and i hashes
-        repeat(' ', n - i);
-        repeat('#', i);
+        // Inner loop for n - i spaces before the hashes
+        for (int j = 1; j <= n - i; j++)
+        {
+            printf(" ");
+        }
 
-        // Print the gap between the two sets of hashes
+        // Inner loop for i left hashes
+        for (int k = 1; k <= i; k++)
+        {
+            printf("#");
+        }
+
+        // Gap between pyramids
         printf("  ");
 
-        // Print the second set of i hashes
-        repeat('#', i);
+        // Inner loop for i right hashes
+        for (int l = 1; l <= i; l++)
+        {
+            printf("#");
+        }
 
-        // Move to the next line after printing each row
+        // Move to the next row
         printf("\n");
     }
-}
 
-// This function repeats a character 'c' 'times' number of times and prints them.
-void repeat(char c, int times)
-{
-    while (--times >= 0)
-    {
-        printf("%c", c);
-    }
+    return 0;
 }
