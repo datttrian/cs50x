bc6f4f4f72c1217fdaeed550dcc03c1ba9f0e94b
Dat Tran <datttrian@gmail.com>
2023-09-09 23:04:42 +0000

    Sat, Sep 9, 2023, 11:04 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,1 +1,29 @@
 # TODO
+import re
+from cs50 import get_string
+
+
+def main():
+    text = get_string("Text: ")
+
+    letters = len(re.findall(r"[a-zA-Z]", text))
+    words = len(text.split())
+    sentences = len(re.findall(r"[.!?]", text))
+
+    L = (letters / words) * 100
+    S = (sentences / words) * 100
+
+    index = 0.0588 * L - 0.296 * S - 15.8
+
+    grade_level = round(index)
+
+    if grade_level < 1:
+        print("Before Grade 1")
+    elif grade_level >= 16:
+        print("Grade 16+")
+    else:
+        print(f"Grade {grade_level}")
+
+
+if __name__ == "__main__":
+    main()
