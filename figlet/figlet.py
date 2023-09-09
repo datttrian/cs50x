16eba60230c60dfb70c93470309c6d7c552565cc
Dat Tran <datttrian@gmail.com>
2023-09-09 11:51:02 +0000

    Sat, Sep 9, 2023, 11:51 AM +00:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,24 +1,30 @@
+# Import the sys module to access command-line arguments and the input function.
+# Import the Figlet class from the pyfiglet library for ASCII art text rendering.
 import sys
 from pyfiglet import Figlet
 
+# Create an instance of the Figlet class.
+figlet = Figlet()
 
-def print_usage_and_exit():
-    print("Invalid usage")
-    sys.exit(1)
-
-
+# Check the number of command-line arguments.
+# If no arguments are provided, prompt the user to input text and render it using the default font.
 if len(sys.argv) == 1:
-    # No command-line arguments, use a random font
-    font_name = None
-elif len(sys.argv) == 3 and (sys.argv[1] == "-f" or sys.argv[1] == "--font"):
-    # Two command-line arguments with -f or --font option, use the specified font
-    font_name = sys.argv[2]
+    text = input("Text: ")
+    print(figlet.renderText(text))
+# If there are exactly three arguments, check if the first argument is "-f" or "--font"
+# and if the second argument is a valid font name from the available fonts.
+elif len(sys.argv) == 3:
+    if (sys.argv[1] == "-f" or sys.argv[1] == "--font") and (
+        sys.argv[2] in figlet.getFonts()
+    ):
+        # Prompt the user to input text and set the font to the specified font.
+        text = input("Text: ")
+        figlet.setFont(font=sys.argv[2])
+        # Render the text using the specified font and print it.
+        print(figlet.renderText(text))
+    else:
+        # If the arguments do not match the expected format, exit the script with an error message.
+        sys.exit("Invalid usage")
 else:
-    print_usage_and_exit()
-    sys.exit(1)
-
-user_text = input("Enter text: ")
-
-figlet = Figlet(font=font_name) if font_name else Figlet()
-output_text = figlet.renderText(user_text)
-print(output_text)
+    # If the number of arguments is not 0 or 3, exit the script with an error message.
+    sys.exit("Invalid usage")
