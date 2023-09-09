e700bb684b4db918d7efe618f50a41cd9208cc07
Dat Tran <datttrian@gmail.com>
2023-09-09 15:53:56 +0000

    Sat, Sep 9, 2023, 3:53 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -11,56 +11,59 @@ int main()
     }
     while (cc_number < 0);
 
-    // Initialize variables for various calculations and tracking digits and sums.
+    // Initialize variables for various calculations and tracking digits and sums
     int digit1 = 0, digit2 = 0, num_digits = 0, sum_of_double_odds = 0, sum_of_evens = 0;
 
-    // Process each digit in the credit card number until there are no more digits.
+    // While there are digits in the credit card number
     while (cc_number > 0)
     {
-        // Store the current digit and the previous digit.
+        // Extract the current digit and the previous digit
         digit2 = digit1;
         digit1 = cc_number % 10;
 
-        // Determine whether the current digit is at an even or odd position.
+        // Determine if the current digit is at an even or odd position
         if (num_digits % 2 == 0)
         {
-            // If the digit is at an even position, add it to the sum of evens.
+            // If it's at an even position, add it to sum_of_evens
             sum_of_evens += digit1;
         }
         else
         {
-            // If the digit is at an odd position, double it and add its digits to the sum of double odds.
+            // If it's at an odd position, double the digit and add its individual digits to sum_of_double_odds
             int multiple = 2 * digit1;
             sum_of_double_odds += (multiple / 10) + (multiple % 10);
         }
 
-        // Move to the next digit in the credit card number and increment the digit count.
+        // Move to the next digit and increment num_digits
         cc_number /= 10;
         num_digits++;
     }
 
-    // Check if the sum of all calculated sums is a multiple of 10 (i.e., if it's a valid credit card number).
+    // Check if the sum of sum_of_evens and sum_of_double_odds is a multiple of 10
     bool is_valid = (sum_of_evens + sum_of_double_odds) % 10 == 0;
 
-    // Extract the first two digits of the credit card number.
+    // Extract the first two digits of the credit card number
     int first_two_digits = (digit1 * 10) + digit2;
 
-    // Check for various credit card company and length conditions and print the result.
+    // Check for conditions to identify the credit card company
     if (digit1 == 4 && num_digits >= 13 && num_digits <= 16 && is_valid)
     {
+        // If the first digit is 4, and the number of digits is between 13 and 16, and it's valid, print "VISA\n"
         printf("VISA\n");
     }
     else if (first_two_digits >= 51 && first_two_digits <= 55 && num_digits == 16 && is_valid)
     {
+        // If the first two digits are between 51 and 55, and the number of digits is 16, and it's valid, print "MASTERCARD\n"
         printf("MASTERCARD\n");
     }
     else if ((first_two_digits == 34 || first_two_digits == 37) && num_digits == 15 && is_valid)
     {
+        // If the first two digits are 34 or 37, and the number of digits is 15, and it's valid, print "AMEX\n"
         printf("AMEX\n");
     }
     else
     {
-        // If none of the conditions are met, the card is invalid.
+        // If none of the above conditions are met, print "INVALID\n"
         printf("INVALID\n");
     }
 }
