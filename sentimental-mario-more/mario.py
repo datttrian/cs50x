8d94fb0ebb16dbfe2f1319740ad290b3cb5c5bda
Dat Tran <datttrian@gmail.com>
2023-09-09 21:39:46 +0000

    Sat, Sep 9, 2023, 9:39 PM +00:00

---
+++ sentimental-mario-more/mario.py
@@ -0,0 +1,23 @@
+# TODO
+from cs50 import get_int
+
+
+def main():
+    # Display "Height: " to prompt the user input a positive integer between 1 and 8 and store it in n
+    while True:
+        n = get_int("Height: ")
+        if 1 <= n <= 8:
+            break
+
+    # For each row from '1' to 'n'
+    for row in range(1, n + 1):
+        spaces = " " * (n - row)
+        left_hashes = "#" * row
+        right_hashes = "#" * row
+
+        # Print 'n - row' spaces, 'row' hash symbols, 'two' spaces, 'row' hash symbols ('#')
+        print(f"{spaces}{left_hashes}  {right_hashes}")
+
+
+if __name__ == "__main__":
+    main()
