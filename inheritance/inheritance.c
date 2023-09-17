adebcd8c610e11263a0083bf8f11395f5e0eaca3
datttrian <datttrian@gmail.com>
2023-09-17 03:36:08 -0700

    Sun, Sep 17, 2023, 10:36 AM +00:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -10,7 +10,8 @@ typedef struct person
 {
     struct person *parents[2];
     char alleles[2];
-} person;
+}
+person;
 
 const int GENERATIONS = 3;
 const int INDENT_LENGTH = 4;
@@ -39,7 +40,6 @@ int main(void)
 person *create_family(int generations)
 {
     // TODO: Allocate memory for new person
-    person *p = malloc(sizeof(person));
 
     // If there are still generations left to create
     if (generations > 1)
@@ -49,47 +49,33 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
-        p->parents[0] = parent0;
-        p->parents[1] = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-        p->alleles[0] = parent0->alleles[rand() % 2];
-        p->alleles[1] = parent1->alleles[rand() % 2];
+
     }
 
     // If there are no generations left to create
     else
     {
         // TODO: Set parent pointers to NULL
-        p->parents[0] = NULL;
-        p->parents[1] = NULL;
 
         // TODO: Randomly assign alleles
-        p->alleles[0] = random_allele();
-        p->alleles[1] = random_allele();
+
     }
 
     // TODO: Return newly created person
-    // return NULL;
-    return p;
+    return NULL;
 }
 
 // Free `p` and all ancestors of `p`.
 void free_family(person *p)
 {
     // TODO: Handle base case
-    if (p == NULL)
-    {
-        return;
-    }
 
     // TODO: Free parents recursively
-    free_family(p->parents[0]);
-    free_family(p->parents[1]);
 
     // TODO: Free child
-    free(p);
-    return;
+
 }
 
 // Print each family member and their alleles.
