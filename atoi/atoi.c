9f086df83d6668b48504c8083c17cf6427e7de66
datttrian <datttrian@gmail.com>
2023-09-17 00:06:03 -0700

    Sun, Sep 17, 2023, 7:06 AM +00:00

--- atoi/atoi.c
+++ atoi/atoi.c
@@ -4,8 +4,6 @@
 #include <stdio.h>
 #include <string.h>
 
-#define ZERO '0' // Define constant ZERO for '0' character
-
 int convert(string input);
 
 int main(void)
@@ -28,18 +26,4 @@ int main(void)
 int convert(string input)
 {
     // TODO
-    // Base case: If the string has only one character, convert and return it
-    if (strlen(input) == 1)
-    {
-        return input[0] - ZERO; // convert char to int
-    }
-    else
-    {
-        // Recursive case: Convert the last character and call convert on the rest of the string
-        int lastDigit = input[strlen(input) - 1] - ZERO; // convert last char to int
-        string shortenedInput = input;
-        shortenedInput[strlen(input) - 1] = '\0';     // shorten the string by removing the last character
-        int remainingValue = convert(shortenedInput); // recursive call
-        return remainingValue * 10 + lastDigit;       // combine the results
-    }
 }
