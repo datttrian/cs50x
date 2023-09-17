306f19a068d4e49c967e30a8dfe699435afa60a0
datttrian <datttrian@gmail.com>
2023-09-16 21:42:01 -0700

    Sun, Sep 17, 2023, 4:42 AM +00:00

--- cash/cash.c
+++ cash/cash.c
@@ -38,29 +38,39 @@ int main(void)
 int get_cents(void)
 {
     // TODO
-    return 0;
+    int cents;
+    do
+    {
+        cents = get_int("Change owed: "); // prompt the user for the change owed
+    }
+    while (cents < 0); // keep asking until until a non-negative value is entered
+    return cents;
 }
 
 int calculate_quarters(int cents)
 {
     // TODO
-    return 0;
+    int quarters = cents / 25; // calculate quarters by dividing cents by 25
+    return quarters;
 }
 
 int calculate_dimes(int cents)
 {
     // TODO
-    return 0;
+    int dimes = (cents % 25) / 10; // calculate dimes using the remainder divided by 10
+    return dimes;
 }
 
 int calculate_nickels(int cents)
 {
     // TODO
-    return 0;
+    int nickels = (cents % 25 % 10) / 5; // calculate nickels using the remainder divided by 5
+    return nickels;
 }
 
 int calculate_pennies(int cents)
 {
     // TODO
-    return 0;
+    int pennies = cents % 5; // equivalent to ((cents % 25) % 10) % 5;
+    return pennies;
 }
