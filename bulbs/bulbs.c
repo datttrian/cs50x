f1d52ca70cfd1bbcbdd60a812e8b231f0bdfb49a
datttrian <datttrian@gmail.com>
2023-09-16 23:24:03 -0700

    Sun, Sep 17, 2023, 6:24 AM +00:00

--- bulbs/bulbs.c
+++ bulbs/bulbs.c
@@ -9,6 +9,24 @@ void print_bulb(int bit);
 int main(void)
 {
     // TODO
+    // Get the message from the user
+    string message = get_string("Message: ");
+
+    // Iterate through each character of the message
+    for (int i = 0; i < strlen(message); i++)
+    {
+        char c = message[i];
+
+        // Convert the character to binary and print it
+        for (int j = BITS_IN_BYTE - 1; j >= 0; j--)
+        {
+            int bit = (c >> j) & 1; // extract the j-th bit of the character using bitwise operator
+            print_bulb(bit);
+        }
+
+        // Print a newline after each byte
+        printf("\n");
+    }
 }
 
 void print_bulb(int bit)
