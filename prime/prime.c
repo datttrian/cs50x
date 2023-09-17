cb4a3892485835c681695527d96bb9860aa74a60
datttrian <datttrian@gmail.com>
2023-09-16 20:42:09 -0700

    Sun, Sep 17, 2023, 3:42 AM +00:00

--- prime/prime.c
+++ prime/prime.c
@@ -16,9 +16,9 @@ int main(void)
     do
     {
         max = get_int("Maximum: ");
-    }
+    } 
     while (min >= max);
-
+    
     for (int i = min; i <= max; i++)
     {
         if (prime(i))
@@ -31,28 +31,5 @@ int main(void)
 bool prime(int number)
 {
     // TODO
-    if (number <= 1)
-    {
-        return false; // numbers less than or equal to 1 are not prime.
-    }
-
-    if (number == 2)
-    {
-        return true; // 2 is a prime number.
-    }
-
-    if (number % 2 == 0)
-    {
-        return false; // Even numbers greater than 2 are not prime.
-    }
-
-    for (int i = 3; i * i <= number; i += 2)
-    {
-        if (number % i == 0)
-        {
-            return false; // If it's divisible by any odd number up to the square root, it's not prime.
-        }
-    }
-
-    return true; // If none of the above conditions were met, the number is prime.
+    return false;
 }
