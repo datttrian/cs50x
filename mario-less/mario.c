486633ff3fa4167fcb99c0d30acaf44b8f549ac7
datttrian <datttrian@gmail.com>
2023-09-16 21:12:19 -0700

    Sun, Sep 17, 2023, 4:12 AM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,7 +1,34 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    // Display "Positive Number: " to prompt the user to input a positive integer from 1 to 8 and store it in n
+    int n;
+    do
+    {
+        n = get_int("Positive Number: ");
+    }
+    while (n < 1 || n > 8);
+
+    // Iterate through each row
+    for (int row = 0; row < n; row++)
+    {
+        // Print 'n - row' spaces
+        for (int i = 0; i < n - row - 1; i++)
+        {
+            printf(" ");
+        }
+
+        // Print 'row' hash symbols ('#')
+        for (int j = 0; j < row + 1; j++)
+        {
+            printf("#");
+        }
+
+        // Move to the next line
+        printf("\n");
+    }
+
+    return 0;
 }
