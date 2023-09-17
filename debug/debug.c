31b261e12ee53fa6789157c97b17884c977c1265
datttrian <datttrian@gmail.com>
2023-09-16 20:18:03 -0700

    Sun, Sep 17, 2023, 3:18 AM +00:00

--- debug/debug.c
+++ debug/debug.c
@@ -2,17 +2,13 @@
 // Learn to debug buggy code
 
 #include <cs50.h>
-#include <stdio.h> // Include the standard I/O library
+
 int main(void)
 {
-    // Declare variables to store name and location
-    string name;
-    string location;
-
     // Ask for your name and where live
-    name = get_string("What is your name? ");
-    location = get_string("Where do you live? ");
+    name = get_string("What is your name? ")
+    location = get_string("Where do you live? ")
 
     // Say hello
-    printf("Hello, %s, from %s!\n", name, location);
+    print("Hello, %i, from %i!", name, location)
 }
