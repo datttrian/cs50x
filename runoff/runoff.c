a20264a8feb353f061c8369dae053e776475b378
datttrian <datttrian@gmail.com>
2023-09-17 01:30:01 -0700

    Sun, Sep 17, 2023, 8:30 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -14,7 +14,8 @@ typedef struct
     string name;
     int votes;
     bool eliminated;
-} candidate;
+}
+candidate;
 
 // Array of candidates
 candidate candidates[MAX_CANDIDATES];
@@ -127,21 +128,6 @@ int main(int argc, string argv[])
 bool vote(int voter, int rank, string name)
 {
     // TODO
-    // Check if the name is a valid candidate name
-    for (int i = 0; i < candidate_count; i++)
-    {
-        // Compare the provided name with the name of each candidate
-        if (strcmp(name, candidates[i].name) == 0)
-        {
-            // Set the preference rank of the current voter to the candidate's index (i)
-            preferences[voter][rank] = i;
-
-            // Indicate a successful vote
-            return true;
-        }
-    }
-
-    // Indicate an invalid candidate name
     return false;
 }
 
@@ -149,50 +135,13 @@ bool vote(int voter, int rank, string name)
 void tabulate(void)
 {
     // TODO
-    // Iterate through each voter's preferences
-    for (int i = 0; i < voter_count; i++)
-    {
-        // Iterate through each rank of preference for the current voter
-        for (int j = 0; j < candidate_count; j++)
-        {
-            // Get the index of the preferred candidate for this rank
-            int preferred_candidate_index = preferences[i][j];
-
-            // Check if the preferred candidate is eliminated
-            if (!candidates[preferred_candidate_index].eliminated)
-            {
-                // If the preferred candidate is not eliminated, increment their vote count
-                candidates[preferred_candidate_index].votes++;
-
-                // Move to the next voter
-                break;
-            }
-        }
-    }
+    return;
 }
 
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
     // TODO
-    // Calculate the number of votes required for a majority
-    int majority_votes = voter_count / 2 + 1;
-
-    // Iterate through each candidate
-    for (int i = 0; i < candidate_count; i++)
-    {
-        // Check if a candidate has received more votes than the majority
-        if (candidates[i].votes >= majority_votes)
-        {
-            // Print the name of the winning candidate
-            printf("%s\n", candidates[i].name);
-
-            // Indicate that a winner has been found
-            return true;
-        }
-    }
-
-    // Indicate no candidate has a majority of votes
     return false;
 }
 
@@ -200,50 +149,19 @@ bool print_winner(void)
 int find_min(void)
 {
     // TODO
-    // Initialize min_votes to a very large value to ensure any candidate's votes will be smaller
-    int min_votes = INT_MAX;
-
-    // Iterate through each candidate
-    for (int i = 0; i < candidate_count; i++)
-    {
-        // Check if the candidate is not eliminated and has fewer votes than the current minimum
-        if (!candidates[i].eliminated && candidates[i].votes < min_votes)
-        {
-            // Update min_votes to the candidate's vote count
-            min_votes = candidates[i].votes;
-        }
-    }
-
-    // Return the minimum vote count among the remaining candidates
-    return min_votes;
+    return 0;
 }
 
 // Return true if the election is tied between all candidates, false otherwise
 bool is_tie(int min)
 {
     // TODO
-    for (int i = 0; i < candidate_count; i++)
-    {
-        if (!candidates[i].eliminated && candidates[i].votes != min)
-        {
-            return false; // Not a tie
-        }
-    }
-    return true; // It's a tie
+    return false;
 }
 
 // Eliminate the candidate (or candidates) in last place
 void eliminate(int min)
 {
     // TODO
-    // Iterate through each candidate
-    for (int i = 0; i < candidate_count; i++)
-    {
-        // Check if the candidate has the same number of votes as the minimum
-        if (candidates[i].votes == min)
-        {
-            // Mark the candidate as eliminated
-            candidates[i].eliminated = true;
-        }
-    }
+    return;
 }
