6abcbc885c7818bfb1aee82adba27c6c3ccd7cd3
datttrian <datttrian@gmail.com>
2023-09-16 21:06:02 -0700

    Sun, Sep 17, 2023, 4:06 AM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,34 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void)
+int main(void) 
 {
-    // Display "Height: " to prompt the user to input a positive integer between 1 and 8 and store it in n
-    int n;
-    do
-    {
-        n = get_int("Height: ");
-    }
-    while (n < 1 || n > 8);
-
-    // For each row from '1' to 'n'
-    for (int row = 1; row <= n; row++)
-    {
-        // Print 'n - row' spaces
-        for (int i = 1; i <= n - row; i++)
-        {
-            printf(" ");
-        }
-
-        // Print 'row' hash symbols ('#') for the pyramid
-        for (int j = 1; j <= row; j++)
-        {
-            printf("#");
-        }
-
-        // Move to the next line for the next row
-        printf("\n");
-    }
-
-    return 0;
+    
 }
