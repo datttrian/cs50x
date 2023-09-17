bc43f419ca90dce90e06d0add6f6cf70fca7fa2d
datttrian <datttrian@gmail.com>
2023-09-16 20:06:13 -0700

    Sun, Sep 17, 2023, 3:06 AM +00:00

--- population/population.c
+++ population/population.c
@@ -4,29 +4,10 @@
 int main(void)
 {
     // TODO: Prompt for start size
-    int start;
-    do
-    {
-        start = get_int("Start size: ");
-    }
-    while (start < 9);
 
     // TODO: Prompt for end size
-    int end;
-    do
-    {
-        end = get_int("End size: ");
-    }
-    while (end < start);
 
     // TODO: Calculate number of years until we reach threshold
-    int years = 0; // keep track of number of years
-    while (start < end)
-    {
-        start = start + (start / 3) - (start / 4);
-        years++;
-    } // update population until we reach threshold
 
     // TODO: Print number of years
-    printf("Years: %i\n", years);
 }
