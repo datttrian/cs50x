775f2b342580fa1c1d7282ed94a0bbaa8a40403c
datttrian <datttrian@gmail.com>
2023-09-16 22:12:07 -0700

    Sun, Sep 17, 2023, 5:12 AM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -1,7 +1,10 @@
-#include <ctype.h>
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
+
+#define UPPERCASE_LETTERS_START 'A'
+#define LOWERCASE_LETTERS_START 'a'
 
 // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
@@ -19,9 +22,38 @@ int main(void)
     int score2 = compute_score(word2);
 
     // TODO: Print the winner
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!\n");
+    }
+    else if (score1 < score2)
+    {
+        printf("Player 2 wins!\n");
+    }
+    else
+    {
+        printf("Tie!\n");
+    }
 }
 
 int compute_score(string word)
 {
     // TODO: Compute and return score for string
+    // Keep track of score
+    int score = 0;
+
+    // Computer score for each character
+    for (int i = 0, len = strlen(word); i < len; i++)
+    {
+        if (isupper(word[i]))
+        {
+            score += POINTS[word[i] - UPPERCASE_LETTERS_START];
+        }
+        else if (islower(word[i]))
+        {
+            score += POINTS[word[i] - LOWERCASE_LETTERS_START];
+        }
+    }
+
+    return score;
 }
