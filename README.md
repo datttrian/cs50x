89a441cac464b3b56affebc2aeacde640de102bf
datttrian <datttrian@gmail.com>
2023-09-23 00:00:01 -0700

    Sat, Sep 23, 2023, 7:00 AM +00:00

--- README.md
+++ README.md
@@ -44,7 +44,7 @@
 
 - Lab: [Songs](songs)
 - Practice Problem: [favorites](favorites), [Hall of Prophecy](prophecy)
-- Problem Set: [Movies](movies), Fiftyville
+- Problem Set: [Movies](movies), [Fiftyville](fiftyville)
 
 ## Week 8 HTML, CSS, JavaScript
 
--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,93 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+-- The THIEF is:
+SELECT people.name
+FROM people
+WHERE people.id IN (
+    -- Get Licence Plates to person IDs
+    SELECT people.id
+    FROM people
+    WHERE people.license_plate IN (
+        SELECT license_plate
+        FROM bakery_security_logs
+        WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10
+        AND minute >= 15 AND minute <=25
+        AND activity = "exit"
+    )
+)
+AND people.id IN (
+    -- Get phone numbers to IDs
+    SELECT people.id
+    FROM people
+    WHERE people.phone_number IN (
+        SELECT caller
+        FROM phone_calls
+        WHERE year = 2021 AND month = 7 AND day = 28
+        AND duration < 60
+    )
+)
+AND people.id IN (
+    -- Get account number to people IDs
+    SELECT people.id
+    FROM people
+    WHERE people.id IN (
+        SELECT person_id
+        FROM bank_accounts
+        WHERE account_number IN (
+            SELECT account_number
+            FROM atm_transactions
+            WHERE year = 2021 AND month = 7 AND day = 28
+            AND atm_location = "Leggett Street"
+            AND transaction_type = "withdraw"
+            ORDER BY amount
+        )
+    )
+)
+AND people.id IN (
+    -- Get Passport Numbers to people IDs
+    SELECT people.id
+    FROM people
+    WHERE people.passport_number IN (
+        SELECT passengers.passport_number
+        FROM people
+        JOIN passengers
+        ON people.passport_number = passengers.passport_number
+        JOIN flights
+        ON passengers.flight_id = flights.id
+        WHERE flights.year = 2021
+        AND flights.month = 7 AND flights.day = 29
+        AND flights.hour = 8  AND flights.minute = 20
+        ORDER BY passengers.passport_number
+    )
+);
+
+-- The city the thief ESCAPED TO:
+SELECT DISTINCT airports.city
+FROM flights
+JOIN airports
+ON airports.id = (
+	SELECT flights.destination_airport_id
+    FROM people
+    JOIN passengers
+    ON people.passport_number = passengers.passport_number
+    JOIN flights
+    ON passengers.flight_id = flights.id
+    WHERE flights.year = 2021
+    AND flights.month = 7 AND flights.day = 29
+    AND people.name = "Bruce"
+);
+
+-- The ACCOMPLICE is:
+SELECT people.name
+FROM people
+WHERE people.phone_number IN (
+	SELECT receiver
+	FROM phone_calls
+	WHERE year = 2021 AND month = 7 AND day = 28
+	AND duration < 60
+	AND caller = (
+	SELECT people.phone_number
+        FROM people
+        WHERE people.name = "Bruce"
+    )
+);
