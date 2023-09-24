f354476b417c4c04f0b49805ef96e55c620de50b
datttrian <datttrian@gmail.com>
2023-09-24 09:42:03 -0700

    Sun, Sep 24, 2023, 4:42 PM +00:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -6,6 +6,15 @@ from flask import Flask, flash, jsonify, redirect, render_template, request, ses
 # Configure application
 app = Flask(__name__)
 
+# TODO
+MONTHS=[]
+for month in range(1,13):
+    MONTHS.append(month)
+
+DAYS = []
+for day in range(1, 32):
+    DAYS.append(day)
+
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
@@ -25,15 +34,34 @@ def after_request(response):
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-
         # TODO: Add the user's entry into the database
-
-        return redirect("/")
+        message = ""
+        name = request.form.get("name")
+        month = request.form.get("month")
+        day = request.form.get("day")
+        if int(month) not in MONTHS:
+            message = "Month is not correct"
+        elif int(day) not in DAYS:
+            message = "Day is not correct"
+        else:
+            db.execute(
+                "INSERT INTO birthdays(name, month, day) VALUES (?, ?,?)",
+                name,
+                month,
+                day,
+            )
+        birthdays = db.execute("SELECT * FROM birthdays")
+        return render_template("index.html", message=message, birthdays=birthdays)
 
     else:
-
         # TODO: Display the entries in the database on index.html
+        birthdays = db.execute("SELECT * FROM birthdays")
+        return render_template("index.html", birthdays=birthdays)
 
-        return render_template("index.html")
 
-
+@app.route("/delete", methods=["POST"])
+def delete():
+    id = request.form.get("id")
+    if id:
+        db.execute("DELETE FROM birthdays WHERE id = ?", id)
+    return redirect("/")
