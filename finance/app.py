467a5614f842a30b8d220ca96d0d194e2217ae35
datttrian <datttrian@gmail.com>
2023-09-24 10:48:30 -0700

    Sun, Sep 24, 2023, 5:48 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -36,19 +36,25 @@ def after_request(response):
 @login_required
 def index():
     """Show portfolio of stocks"""
-    # return apology("TODO")
+    # Retrieve owned stocks for the user from the database
     owned = db.execute(
         "SELECT stock,number FROM owned WHERE user_id = ?", session["user_id"]
     )
     total = 0
+
+    # Calculate the total value of the user's portfolio
     for stock in owned:
         stock["price"] = lookup(stock["stock"])["price"]
         stock["value"] = stock["price"] * stock["number"]
         total += stock["value"]
         stock["value"] = usd(stock["price"] * stock["number"])
+
+    # Retrieve the user's available cash balance
     cash = db.execute("SELECT cash FROM users WHERE id=?", session["user_id"])
     total += cash[0]["cash"]
     total = usd(total)
+
+    # Render the portfolio page with relevant data
     return render_template(
         "index.html", owned=owned, cash=usd(cash[0]["cash"]), total=total
     )
@@ -58,8 +64,9 @@ def index():
 @login_required
 def buy():
     """Buy shares of stock"""
-    # return apology("TODO")
+    # Handle POST request when user submits a buy request
     if request.method == "POST":
+        # Check if all required form fields are provided
         if not request.form.get("symbol"):
             return apology("must provide a stock", 400)
         elif not lookup(request.form.get("symbol")):
@@ -70,18 +77,25 @@ def buy():
             return apology("must enter a number", 400)
         elif int(request.form.get("shares")) <= 0:
             return apology("must buy at least one", 400)
+
+        # Retrieve stock and price information
         stock = request.form.get("symbol")
         price = lookup(request.form.get("symbol"))["price"] * int(
             request.form.get("shares")
         )
+
+        # Check if the user has enough funds to make the purchase
         rows = db.execute("SELECT cash from users WHERE id = ?", session["user_id"])
         if price > rows[0]["cash"]:
             return apology("not enough funds", 400)
         else:
+            # Update the user's cash balance
             cash = rows[0]["cash"] - price
             db.execute(
                 "UPDATE users SET cash = ? WHERE id = ?", cash, session["user_id"]
             )
+
+            # Update the user's stock portfolio or create a new entry
             data = db.execute(
                 "SELECT number FROM owned WHERE user_id = ? AND stock = ?",
                 session["user_id"],
@@ -101,6 +115,8 @@ def buy():
                     request.form.get("symbol"),
                     int(request.form.get("shares")),
                 )
+
+            # Record the transaction in the 'transactions' table
             now = datetime.now()
             date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
             db.execute(
@@ -111,8 +127,11 @@ def buy():
                 date_time,
                 int(request.form.get("shares")),
             )
+
+            # Redirect the user to the portfolio page
             return redirect("/")
     else:
+        # Render the buy page if it's a GET request
         return render_template("buy.html")
 
 
@@ -120,10 +139,12 @@ def buy():
 @login_required
 def history():
     """Show history of transactions"""
-    # return apology("TODO")
+    # Retrieve the user's transaction history from the database
     transactions = db.execute(
         "SELECT * FROM transactions WHERE user_id = ?", session["user_id"]
     )
+
+    # Render the transaction history page with relevant data
     return render_template("history.html", transactions=transactions)
 
 
@@ -136,7 +157,6 @@ def login():
 
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
-
         # Ensure username was submitted
         if not request.form.get("username"):
             return apology("must provide username", 403)
@@ -146,10 +166,14 @@ def login():
             return apology("must provide password", 403)
 
         # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
 
         # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["hash"], request.form.get("password")
+        ):
             return apology("invalid username and/or password", 403)
 
         # Remember which user has logged in
@@ -178,24 +202,25 @@ def logout():
 @login_required
 def quote():
     """Get stock quote."""
-    # return apology("TODO")
+    # Handle POST request when user submits a quote request
     if request.method == "POST":
         if not request.form.get("symbol"):
             return apology("must provide a stock symbol", 400)
         if lookup(request.form.get("symbol")):
+            # Retrieve stock information and format the price
             stock = lookup(request.form.get("symbol"))
             stock["price"] = usd(stock["price"])
             return render_template("quoted.html", stock=stock)
         else:
             return apology("No such stock symbol", 400)
     else:
+        # Render the quote page if it's a GET request
         return render_template("quote.html")
 
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
-    # return apology("TODO")
     if request.method == "POST":
         # Ensure username was submitted
         if not request.form.get("username"):
@@ -207,11 +232,15 @@ def register():
             return apology("must confirm your password", 400)
         elif request.form.get("confirmation") != request.form.get("password"):
             return apology("must check your password again", 400)
+
+        # Check if the username is already in use
         rows = db.execute(
             "SELECT * FROM users WHERE username = ?", request.form.get("username")
         )
         if rows:
             return apology("username already used", 400)
+
+        # Insert the new user into the 'users' table and log them in
         db.execute(
             "INSERT INTO users (username,hash) VALUES (?,?)",
             request.form.get("username"),
@@ -223,6 +252,7 @@ def register():
         session["user_id"] = number[0]["id"]
         return redirect("/")
     else:
+        # Render the registration page if it's a GET request
         return render_template("register.html")
 
 
@@ -230,7 +260,6 @@ def register():
 @login_required
 def sell():
     """Sell shares of stock"""
-    # return apology("TODO")
     owned = db.execute(
         "SELECT stock,number FROM owned WHERE user_id = ?", session["user_id"]
     )
@@ -246,6 +275,8 @@ def sell():
             return apology("must enter a number", 400)
         elif int(request.form.get("shares")) <= 0:
             return apology("must enter a positive number", 400)
+
+        # Loop through the owned stocks to find the selected one
         for stock in owned:
             if stock["stock"] == request.form.get("symbol"):
                 if stock["number"] == 0:
@@ -253,6 +284,7 @@ def sell():
                 elif stock["number"] < int(request.form.get("shares")):
                     return apology("you don't have enough", 400)
                 elif stock["number"] == int(request.form.get("shares")):
+                    # Sell all shares and update cash balance
                     now = datetime.now()
                     date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
                     db.execute(
@@ -278,6 +310,7 @@ def sell():
                         int(request.form.get("shares")),
                     )
                 else:
+                    # Sell some shares and update cash balance
                     now = datetime.now()
                     date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
                     cash += (
@@ -305,4 +338,5 @@ def sell():
                     )
         return redirect("/")
     else:
+        # Render the sell page if it's a GET request
         return render_template("sell.html", owned=owned)
